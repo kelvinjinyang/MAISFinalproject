801a2de56b25289d4bdbebc1fea7955f89a5b854
Kelvin Jin-Yang <kelvin.jin-yang@mail.mcgill.ca>
2019-10-05 22:41:13 -0400

    Create environment.py

---
+++ environment.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import gym
+
+gym-foo/
+    README.md
+    setup.py
+    gym_foo/
+        __init__.py
+            envs/
+                __init__.py
+                foo_env.py
+
